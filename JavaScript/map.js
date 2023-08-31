2c9d4d5ef32fc5f450a61376b6f67b8979a096d4
davidwebdeveloper <mdd11024@gmail.com>
2023-08-31 11:43:51 +0530

    squared of each number to new array

--- JavaScript/map.js
+++ JavaScript/map.js
@@ -1,2 +1,9 @@
 // First program of javaScript
 console.log("hello world");
+
+// Write a function that takes an array of numbers and returns a new array with each number squared.
+// Use the Map object to accomplish this.
+
+const a = [4, 5, 6];
+const square = a.map((e) => e * e);
+console.log(square);
