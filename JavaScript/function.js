da430e7ea95971f5699581575998ec1082c67b5a
davidwebdeveloper <mdd11024@gmail.com>
2023-08-31 16:50:15 +0530

    Arrow-function and Function Expression

---
+++ JavaScript/function.js
@@ -0,0 +1,33 @@
+// calculate Age
+
+// Normal function
+
+// function calcAge(birthYear) {
+//   return 2023 - birthYear;
+// }
+
+// console.log(calcAge(2002));
+
+// Function Expression
+
+// const ageCalc = function (birthYear) {
+//   return 2023 - birthYear;
+// };
+
+// console.log(ageCalc(2002));
+
+// Arrow Function
+
+// const calcAge = (birthYear) => 2023 - birthYear;
+// console.log(calcAge(2002));
+
+const yearsLeftRetirement = (birthYear, name) => {
+  const age = 2023 - birthYear;
+  const retirement = 60 - age;
+  return `${name} retires in ${retirement} years.`;
+};
+
+console.log(yearsLeftRetirement(2002, "david"));
+console.log(yearsLeftRetirement(2000, "ajith"));
+console.log(yearsLeftRetirement(1998, "ranjan"));
+console.log(yearsLeftRetirement(1991, "paul"));
