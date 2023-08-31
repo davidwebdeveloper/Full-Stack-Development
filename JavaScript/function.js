2e74b4c876ee8ffcb0308800375910fe04383c3e
davidwebdeveloper <mdd11024@gmail.com>
2023-08-31 16:59:22 +0530

    Apple juice program using function

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -21,13 +21,25 @@
 // const calcAge = (birthYear) => 2023 - birthYear;
 // console.log(calcAge(2002));
 
-const yearsLeftRetirement = (birthYear, name) => {
-  const age = 2023 - birthYear;
-  const retirement = 60 - age;
-  return `${name} retires in ${retirement} years.`;
+// const yearsLeftRetirement = (birthYear, name) => {
+//   const age = 2023 - birthYear;
+//   const retirement = 60 - age;
+//   return `${name} retires in ${retirement} years.`;
+// };
+
+// console.log(yearsLeftRetirement(2002, "david"));
+// console.log(yearsLeftRetirement(2000, "ajith"));
+// console.log(yearsLeftRetirement(1998, "ranjan"));
+// console.log(yearsLeftRetirement(1991, "paul"));
+
+const cutPieces = function (fruit) {
+  return fruit * 4;
 };
 
-console.log(yearsLeftRetirement(2002, "david"));
-console.log(yearsLeftRetirement(2000, "ajith"));
-console.log(yearsLeftRetirement(1998, "ranjan"));
-console.log(yearsLeftRetirement(1991, "paul"));
+const fruitProcessor = function (apples, oranges) {
+  const applePieces = cutPieces(apples);
+  const orangePieces = cutPieces(oranges);
+  return `Juice made with ${applePieces} apple pieces and ${orangePieces} orange pieces`;
+};
+
+console.log(fruitProcessor(5, 10));
