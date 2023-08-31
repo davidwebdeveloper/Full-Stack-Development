b877b063b3d6df755b74a1168a5847d49a76cca0
davidwebdeveloper <mdd11024@gmail.com>
2023-08-31 12:23:50 +0530

    map problem solved

---
+++ JavaScript/map/002-capitalize.js
@@ -0,0 +1,9 @@
+const myFriends = [
+  { name: "david" },
+  { name: "vetrivel" },
+  { name: "pravin abdul kalam" },
+];
+const myFriendsCapitalize = myFriends.map(
+  (e) => e.name.charAt(0).toUpperCase() + e.name.slice(1)
+);
+console.log(myFriendsCapitalize);
